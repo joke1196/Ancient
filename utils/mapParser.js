c6f4d02fcc3ffd6982735a39d67b9db544833b25
David Kunzmann <davidkunzmann@users.noreply.github.com>
2015-10-03 17:52:18 +0200

    Clean up. Game map are loaded as json file now

---
+++ utils/mapParser.js
@@ -0,0 +1,20 @@
+function getFile(level) {
+  var client = new XMLHttpRequest();
+  client.open('GET', 'assets/map/' + level, false);
+  client.onreadystatechange = function() {
+    return client.responseText;
+  }
+  client.send();
+  return parserMap(client.onreadystatechange());
+}
+
+function parserMap(txtData) {
+  var jsonData = JSON.parse(txtData);
+  console.log(jsonData);
+  // var mapArray = [];
+  // for (var index in jsonData.map) {
+  //   console.log(jsonData.map[index]);
+  //   mapArray.push(jsonData.map[index]);
+  // }
+  return jsonData.map;
+}
