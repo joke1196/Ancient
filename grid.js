298ccedfc6f345aa225618449c2292a44ff2b592
David Kunzmann <davidkunzmann@users.noreply.github.com>
2015-10-15 16:11:49 +0200

    Modified position of image for the tile

--- grid.js
+++ grid.js
@@ -1,6 +1,6 @@
 function Grid(layout, level, mapArray){
   this.layout = layout;
-  this.X_OFFSET = this.layout.size.x - 7;
+  this.X_OFFSET = this.layout.size.x -7;
   this.mapArray = mapArray;
   this.level = level;
   this.hashMap = new Map();
@@ -23,7 +23,7 @@ Grid.prototype.getHashMap = function(){
 Grid.prototype.draw = function(ctx){
     for(var index in this.polygons){
       var value = this.polygons[index].value;
-      ctx.drawImage(this.textures[value], this.polygons[index].poly[4].x - this.X_OFFSET,this.polygons[index].poly[4].y, (this.textures[value].width)*(0.575), ((this.textures[value].height)*(1/3)));
+      ctx.drawImage(this.textures[value], this.polygons[index].poly[4].x - this.X_OFFSET,this.polygons[index].poly[4].y, (this.textures[value].width)*(0.58), ((this.textures[value].height)*(0.31)));
       ctx.beginPath();
       for(var i in this.polygons[index].poly){
         ctx.lineTo(this.polygons[index].poly[i].x ,this.polygons[index].poly[i].y);
